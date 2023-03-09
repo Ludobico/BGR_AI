69169c6607dcd4522899c25029950418afeeef32
Ludobico <aqs450@gmail.com>
2023-03-09 13:25:24 +0900

    AnnoFixer

--- Utils/AnnoFixer.py
+++ Utils/AnnoFixer.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import json
 
 
 class AnnoFixer:
@@ -26,3 +27,36 @@ class AnnoFixer:
             return
         else:
             print('train 폴더 check')
+
+        self.fixCOCO()
+
+    def fixCOCO(self, intend=2):
+        self.mergedir = os.path.join(self.resultdir, 'AnnotationFixed')
+
+        if not os.path.exists(self.mergedir):
+            os.mkdir(self.mergedir)
+        else:
+            print('수정된 Annotation 파일 확인')
+            print(self.mergedir)
+
+        for file_name in os.listdir(self.labeldir):
+            if not file_name.endswith('.json'):
+                continue
+
+            file_path = os.path.join(self.labeldir, file_name)
+            with open(file_path, 'r') as f:
+                data = json.load(f)
+
+            data['images'] = [data['images']]
+
+            fixed_file_name = os.path.splitext(file_name)[0] + '_fixed.json'
+            new_file_path = os.path.join(self.mergedir, fixed_file_name)
+
+            with open(new_file_path, 'w') as f:
+                json.dump(data, f, indent=2)
+
+        print("Annotation JSON 파일 수정 완료")
+
+
+if __name__ == "__main__":
+    AnnoFixer()
