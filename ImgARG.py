5553ae7f658ff0c3edb8d4260e31c9a6434fc16f
Ludobico <aqs450@gmail.com>
2023-03-09 16:43:49 +0900

    disease classes update

--- ImgARG.py
+++ ImgARG.py
@@ -41,47 +41,82 @@ class ImageAnalysis:
 
         for k, v in self.countDict.items():
             # print(k, v)
-            # 질병 클래스
+            # 정상 클래스
             if 'PO' in k:
                 v = str(v)+str('\t 정상')
+                if 'OUN' in k:
+                    v = str(v)+str('\t 외형')
+                if 'DYN' in k:
+                    v = str(v)+str('\t 체표')
+                if 'NAN' in k:
+                    v = str(v)+str('\t 항문')
+                if 'FDN' in k:
+                    v = str(v)+str('\t 등지느러미')
+                if 'FAN' in k:
+                    v = str(v)+str('\t 뒷지느러미')
+                if 'FCN' in k:
+                    v = str(v)+str('\t 꼬리지느러미')
+                if 'EYN' in k:
+                    v = str(v)+str('\t 눈')
+                if 'MON' in k:
+                    v = str(v)+str('\t 입')
+                if 'BEN' in k:
+                    v = str(v)+str('\t 복부')
+                if 'GCN' in k:
+                    v = str(v)+str('\t 아가미뚜껑')
+                if 'GIN' in k:
+                    v = str(v)+str('\t 아가미뚜껑/아가미')
+                if 'LIN' in k:
+                    v = str(v)+str('\t 간')
+                if 'INN' in k:
+                    v = str(v)+str('\t 장')
+                if 'SPN' in k:
+                    v = str(v)+str('\t 비장')
+                if 'ASN' in k:
+                    v = str(v)+str('\t 복수')
+                if 'REN' in k:
+                    v = str(v)+str('\t 생식소')
+                if 'KIN' in k:
+                    v = str(v)+str('\t 신장')
+            # 비브리오증 클래스
             if 'VI' in k:
                 v = str(v)+str('\t 비브리오증')
-
-            # 질병 부위 클래스
-            if 'OUN' in k:
-                v = str(v)+str('\t 외형')
-            if 'DYN' in k:
-                v = str(v)+str('\t 체표')
-            if 'NAN' in k:
-                v = str(v)+str('\t 항문')
-            if 'FDN' in k:
-                v = str(v)+str('\t 등지느러미')
-            if 'FAN' in k:
-                v = str(v)+str('\t 뒷지느러미')
-            if 'FCN' in k:
-                v = str(v)+str('\t 꼬리지느러미')
-            if 'EYN' in k:
-                v = str(v)+str('\t 눈')
-            if 'MON' in k:
-                v = str(v)+str('\t 입')
-            if 'BEN' in k:
-                v = str(v)+str('\t 복부')
-            if 'GCN' in k:
-                v = str(v)+str('\t 아가미뚜껑')
-            if 'GIN' in k:
-                v = str(v)+str('\t 아가미뚜껑/아가미')
-            if 'LIN' in k:
-                v = str(v)+str('\t 간')
-            if 'INN' in k:
-                v = str(v)+str('\t 장')
-            if 'SPN' in k:
-                v = str(v)+str('\t 비장')
-            if 'ASN' in k:
-                v = str(v)+str('\t 복수')
-            if 'REN' in k:
-                v = str(v)+str('\t 생식소')
-            if 'KIN' in k:
-                v = str(v)+str('\t 신장')
+                if 'DYR' in k:
+                    v = str(v)+str('\t 체표 발적')
+                if 'DYN' in k:
+                    v = str(v)+str('\t 체표 괴사')
+                if 'DYA' in k:
+                    v = str(v)+str('\t 체표 근육 출혈')
+                if 'MOU' in k:
+                    v = str(v)+str('\t 주둥이 궤양')
+                if 'GIP' in k:
+                    v = str(v)+str('\t 아가미 빈혈')
+                if 'FDS' in k:
+                    v = str(v)+str('\t 등지느러미 갈라짐')
+                if 'FAS' in k:
+                    v = str(v)+str('\t 뒷지느러미 갈라짐')
+                if 'FCS' in k:
+                    v = str(v)+str('\t 꼬리지느러미 갈라짐')
+                if 'FDH' in k:
+                    v = str(v)+str('\t 등지느러미 출혈')
+                if 'FAH' in k:
+                    v = str(v)+str('\t 뒷지느러미 출혈')
+                if 'FCH' in k:
+                    v = str(v)+str('\t 꼬리지느러미 출혈')
+                if 'FDN' in k:
+                    v = str(v)+str('\t 등지느러미 괴사')
+                if 'FAN' in k:
+                    v = str(v)+str('\t 뒷지느러미 괴사')
+                if 'FCN' in k:
+                    v = str(v)+str('\t 꼬리지느러미 괴사')
+                if 'LIC' in k:
+                    v = str(v)+str('\t 간 울혈')
+                if 'LII' in k:
+                    v = str(v)+str('\t 간 염증')
+                if 'KIE' in k:
+                    v = str(v)+str('\t 신장 비대')
+                if 'SPE' in k:
+                    v = str(v)+str('\t 비장 비대')
 
             print(k, v)
 
